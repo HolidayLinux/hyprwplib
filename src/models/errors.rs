9d92bcc1c14f0ca54477212d7262e448089aee01
Holiday <alega00@mail.ru>
2025-11-19 16:34:41 +0800

    Part of errors refactor

--- src/models/errors.rs
+++ src/models/errors.rs
@@ -7,4 +7,18 @@ impl Error for NotCorrectPathError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Not correct path.")
     }
+
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        self.source()
+    }
+
+    fn provide<'a>(&'a self, request: &mut std::error::Request<'a>) {}
 }
